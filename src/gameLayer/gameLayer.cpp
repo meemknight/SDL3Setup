2eed3625a8d276fe03348a6f6c8ee26ef0e79d84
vlod <vladalex420@gmail.com>
2025-12-26 16:28:55 +0200

    first commit

---
+++ src/gameLayer/gameLayer.cpp
@@ -0,0 +1,122 @@
+#define GLM_ENABLE_EXPERIMENTAL
+#include "gameLayer.h"
+#include <glm/glm.hpp>
+#include <glm/gtx/transform.hpp>
+#include "platformInput.h"
+#include "imgui.h"
+#include <iostream>
+#include <sstream>
+#include <platformTools.h>
+#include <imguiTools.h>
+#include <logs.h>
+#include <SDL3/SDL.h>
+#include <gl2d/gl2d.h>
+
+
+struct GameData
+{
+	glm::vec2 rectPos = {100,100};
+
+}gameData;
+
+
+gl2d::Renderer2D renderer;
+gl2d::Camera camera;
+
+bool initGame(SDL_Renderer *sdlRenderer)
+{
+
+	renderer.create(sdlRenderer);
+
+
+	return true;
+}
+
+
+//IMPORTANT NOTICE, IF YOU WANT TO SHIP THE GAME TO ANOTHER PC READ THE README.MD IN THE GITHUB
+//https://github.com/meemknight/cmakeSetup
+//OR THE INSTRUCTION IN THE CMAKE FILE.
+//YOU HAVE TO CHANGE A FLAG IN THE CMAKE SO THAT RESOURCES_PATH POINTS TO RELATIVE PATHS
+//BECAUSE OF SOME CMAKE PROGBLMS, RESOURCES_PATH IS SET TO BE ABSOLUTE DURING PRODUCTION FOR MAKING IT EASIER.
+
+bool gameLogic(float deltaTime, platform::Input &input, SDL_Renderer *sdlRenderer)
+{
+#pragma region init stuff
+	int w = 0; int h = 0;
+	w = platform::getFrameBufferSizeX(); //window w
+	h = platform::getFrameBufferSizeY(); //window h
+	
+	renderer.updateWindowMetrics(w, h);
+
+	//SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+	//SDL_RenderClear(renderer);
+
+#pragma endregion
+
+	//you can also do platform::isButtonHeld(platform::Button::Left)
+
+	if (input.isButtonHeld(platform::Button::Left))
+	{
+		gameData.rectPos.x -= deltaTime * 100;
+	}
+	if (input.isButtonHeld(platform::Button::Right))
+	{
+		gameData.rectPos.x += deltaTime * 100;
+	}
+	if (input.isButtonHeld(platform::Button::Up))
+	{
+		gameData.rectPos.y -= deltaTime * 100;
+	}
+	if (input.isButtonHeld(platform::Button::Down))
+	{
+		gameData.rectPos.y += deltaTime * 100;
+	}
+
+
+
+	//ImGui::ShowDemoWindow();
+	ImGui::PushMakeWindowNotTransparent();
+	ImGui::Begin("Test Imgui");
+
+	ImGui::DragFloat2("Positions", &gameData.rectPos[0]);
+
+
+	ImGui::helpMarker("test");
+
+	ImGui::addErrorSymbol();
+	ImGui::addWarningSymbol();
+
+
+	ImGui::PopMakeWindowNotTransparent();
+	ImGui::End();
+
+
+
+	renderer.renderRectangle({gameData.rectPos, 100, 100}, Colors_Blue);
+
+
+
+	renderer.flush();
+
+
+	//SDL_FRect square = {};
+	//square.x = gameData.rectPos.x;
+	//square.y = gameData.rectPos.y;
+	//square.w = 100;
+	//square.h = 100;
+	//SDL_SetRenderDrawColor(sdlRenderer, 255, 0, 0, 255);
+	//SDL_RenderFillRect(sdlRenderer, &square);
+
+
+	return true;
+#pragma endregion
+
+}
+
+//This function might not be be called if the program is forced closed
+void closeGame()
+{
+
+
+
+}
---
+++ src/platform/SDL3Main.cpp
@@ -0,0 +1,263 @@
+#define SDL_MAIN_HANDLED
+#include <SDL3/SDL.h>
+
+#include <iostream>
+#include <chrono>
+#include <fstream>
+
+#include "platformTools.h"
+#include "platformInput.h"
+#include "gameLayer.h"
+#include "stringManipulation.h"
+
+#include "imgui.h"
+#include "backends/imgui_impl_sdl3.h"
+#include "backends/imgui_impl_sdlrenderer3.h"
+#include "imguiThemes.h"
+
+#undef min
+#undef max
+
+#pragma region globals
+static SDL_Window *window = nullptr;
+static SDL_Renderer *sdlRenderer = nullptr;
+
+bool currentFullScreen = false;
+bool fullScreen = false;
+bool windowFocus = true;
+int mouseMovedFlag = 0;
+#pragma endregion
+
+
+namespace platform
+{
+	void setRelMousePosition(int x, int y)
+	{
+		SDL_WarpMouseInWindow(window, (float)x, (float)y);
+	}
+
+	bool isFullScreen() { return fullScreen; }
+	void setFullScreen(bool f) { fullScreen = f; }
+
+	glm::ivec2 getFrameBufferSize()
+	{
+		int w, h;
+		SDL_GetWindowSizeInPixels(window, &w, &h);
+		return {w, h};
+	}
+
+	glm::ivec2 getRelMousePosition()
+	{
+		float x, y;
+		SDL_GetMouseState(&x, &y);
+		return {(int)x, (int)y};
+	}
+
+	glm::ivec2 getWindowSize()
+	{
+		int w, h;
+		SDL_GetWindowSize(window, &w, &h);
+		return {w, h};
+	}
+
+	void showMouse(bool show)
+	{
+		//SDL_SetCursorVisible(show); TODO
+	}
+
+	bool hasFocused() { return windowFocus; }
+	bool mouseMoved() { return mouseMovedFlag; }
+
+	// ----- file helpers unchanged -----
+	bool writeEntireFile(const char *name, void *buffer, size_t size)
+	{
+		std::ofstream f(name, std::ios::binary);
+		if (!f.is_open()) return false;
+		f.write((char *)buffer, size);
+		return true;
+	}
+}
+
+// ==============================
+// SDL input translation
+// ==============================
+static void handleSDLEvent(const SDL_Event &e)
+{
+	switch (e.type)
+	{
+	case SDL_EVENT_QUIT:
+	exit(0);
+
+	case SDL_EVENT_WINDOW_FOCUS_GAINED:
+	windowFocus = true;
+	break;
+
+	case SDL_EVENT_WINDOW_FOCUS_LOST:
+	windowFocus = false;
+	platform::internal::resetInputsToZero();
+	break;
+
+	case SDL_EVENT_MOUSE_MOTION:
+	mouseMovedFlag = 1;
+	break;
+
+	case SDL_EVENT_MOUSE_BUTTON_DOWN:
+	case SDL_EVENT_MOUSE_BUTTON_UP:
+	{
+		bool state = (e.type == SDL_EVENT_MOUSE_BUTTON_DOWN);
+		if (e.button.button == SDL_BUTTON_LEFT)
+			platform::internal::setLeftMouseState(state);
+		if (e.button.button == SDL_BUTTON_RIGHT)
+			platform::internal::setRightMouseState(state);
+	} break;
+
+	case SDL_EVENT_KEY_DOWN:
+	case SDL_EVENT_KEY_UP:
+	{
+		bool state = (e.type == SDL_EVENT_KEY_DOWN);
+
+		SDL_Keycode key = e.key.key;
+		//SDL_Keymod  mod = e.key.mod;
+		//SDL_Scancode sc = e.key.scancode;
+
+		if (key >= SDLK_A && key <= SDLK_Z)
+			platform::internal::setButtonState(
+			platform::Button::A + (key - SDLK_A), state);
+
+		else if (key >= SDLK_0 && key <= SDLK_9)
+			platform::internal::setButtonState(
+			platform::Button::NR0 + (key - SDLK_0), state);
+
+		else
+		{
+			if (key == SDLK_SPACE) platform::internal::setButtonState(platform::Button::Space, state);
+			if (key == SDLK_RETURN) platform::internal::setButtonState(platform::Button::Enter, state);
+			if (key == SDLK_ESCAPE) platform::internal::setButtonState(platform::Button::Escape, state);
+			if (key == SDLK_UP) platform::internal::setButtonState(platform::Button::Up, state);
+			if (key == SDLK_DOWN) platform::internal::setButtonState(platform::Button::Down, state);
+			if (key == SDLK_LEFT) platform::internal::setButtonState(platform::Button::Left, state);
+			if (key == SDLK_RIGHT) platform::internal::setButtonState(platform::Button::Right, state);
+			if (key == SDLK_LCTRL) platform::internal::setButtonState(platform::Button::LeftCtrl, state);
+			if (key == SDLK_TAB) platform::internal::setButtonState(platform::Button::Tab, state);
+			if (key == SDLK_LSHIFT) platform::internal::setButtonState(platform::Button::LeftShift, state);
+			if (key == SDLK_LALT) platform::internal::setButtonState(platform::Button::LeftAlt, state);
+		}
+	} break;
+
+	case SDL_EVENT_TEXT_INPUT:
+	{
+		char c = e.text.text[0];
+		if (c < 127)
+			platform::internal::addToTypedInput(c);
+	} break;
+	}
+}
+
+int main(int, char **)
+{
+	permaAssertComment(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS),
+		"SDL init failed");
+
+	window = SDL_CreateWindow(
+		"geam",
+		500, 500,
+		SDL_WINDOW_RESIZABLE
+	);
+
+	permaAssertComment(window, "SDL window creation failed");
+
+	sdlRenderer = SDL_CreateRenderer(window, nullptr);
+	
+	permaAssertComment(sdlRenderer, "SDL renderer creation failed");
+
+
+#pragma region imgui
+	ImGui::CreateContext();
+	imguiThemes::embraceTheDarkness();
+
+	ImGuiIO &io = ImGui::GetIO();
+	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+	ImGuiStyle &style = ImGui::GetStyle();
+	style.Colors[ImGuiCol_WindowBg].w = 0.5f;
+	style.FontScaleMain = 2.5;
+
+	ImGui_ImplSDL3_InitForSDLRenderer(window, sdlRenderer);
+	ImGui_ImplSDLRenderer3_Init(sdlRenderer);
+#pragma endregion
+
+
+	if (!initGame(sdlRenderer))
+	{
+		return 0;
+	}
+
+	auto last = std::chrono::high_resolution_clock::now();
+
+	while (true)
+	{
+		SDL_Event e;
+		while (SDL_PollEvent(&e))
+		{
+			ImGui_ImplSDL3_ProcessEvent(&e);
+			handleSDLEvent(e);
+		}
+
+
+		auto now = std::chrono::high_resolution_clock::now();
+		float dt = std::chrono::duration<float>(now - last).count();
+		last = now;
+		dt = std::min(dt, 1.f / 10.f);
+
+	#pragma region imgui
+		ImGui_ImplSDLRenderer3_NewFrame();
+		ImGui_ImplSDL3_NewFrame();
+		ImGui::NewFrame();
+
+		ImGui::PushStyleColor(ImGuiCol_WindowBg, {});
+		ImGui::PushStyleColor(ImGuiCol_DockingEmptyBg, {});
+		ImGui::DockSpaceOverViewport();
+		ImGui::PopStyleColor(2);
+	#pragma endregion
+
+		platform::Input input = {};
+		input.deltaTime = dt;
+		input.hasFocus = platform::hasFocused();
+		memcpy(input.buttons, platform::getAllButtons(), sizeof(input.buttons));
+		input.mouseX = platform::getRelMousePosition().x;
+		input.mouseY = platform::getRelMousePosition().y;
+		input.lMouse = platform::getLMouseButton();
+		input.rMouse = platform::getRMouseButton();
+		strlcpy(input.typedInput, platform::getTypedInput(), sizeof(input.typedInput));
+
+		SDL_SetRenderDrawColor(sdlRenderer, 0, 0, 0, 255);
+		SDL_RenderClear(sdlRenderer);
+
+		if (!gameLogic(dt, input, sdlRenderer))
+		{
+			break;
+		}
+
+		mouseMovedFlag = 0;
+		platform::internal::updateAllButtons(dt);
+		platform::internal::resetTypedInput();
+
+
+		ImGui::Render();
+		ImGui_ImplSDLRenderer3_RenderDrawData(
+			ImGui::GetDrawData(), sdlRenderer);
+
+
+		SDL_RenderPresent(sdlRenderer);
+	}
+
+	closeGame();
+
+	ImGui_ImplSDLRenderer3_Shutdown();
+	ImGui_ImplSDL3_Shutdown();
+	ImGui::DestroyContext();
+
+	SDL_DestroyRenderer(sdlRenderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+}
